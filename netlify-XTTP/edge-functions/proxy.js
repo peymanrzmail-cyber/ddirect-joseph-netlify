1668dd5337c3e1181f0b9f13b849f326d3106399
peymanrzmail-cyber <peymanrzmail@gmail.com>
2026-05-03 01:09:38 +0330

    Update proxy.js
    New Port

--- netlify-XTTP/edge-functions/proxy.js
+++ netlify-XTTP/edge-functions/proxy.js
@@ -1,6 +1,6 @@
 // netlify/edge-functions/proxy.js
 
-const BACKEND_URL = Netlify.env.get("BACKEND_URL") || "https://us1.darkcloud.ir:4430";
+const BACKEND_URL = Netlify.env.get("BACKEND_URL") || "https://us1.darkcloud.ir:443";
 
 export default async function handler(request, context) {
   try {
